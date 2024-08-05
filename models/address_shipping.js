fe2b3bab5f9a49a3e9a17464a4335645f2d31594
Widy Guilias <widyguilias@gmail.com>
2024-08-05 15:59:18 +0700

    add validation for address_shipping model

--- models/address_shipping.js
+++ models/address_shipping.js
@@ -14,16 +14,105 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Address_Shipping.init({
-    id_user: DataTypes.UUID,
-    province: DataTypes.STRING,
-    city: DataTypes.STRING,
-    district: DataTypes.STRING,
-    village: DataTypes.STRING,
-    code_post: DataTypes.STRING,
-    detail_address: DataTypes.STRING,
-    nickname_address: DataTypes.STRING,
-    latitude: DataTypes.STRING,
-    longitude: DataTypes.STRING
+    id_user: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true
+      }
+    },
+    province: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        is: /^[a-zA-Z .,]*$/,
+        len: [5,24]
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        is: /^[a-zA-Z .,]*$/,
+        len: [5,24]
+      }
+    },
+    district: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        is: /^[a-zA-Z .,]*$/,
+        len: [5,24]
+      }
+    },
+    village: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        is: /^[a-zA-Z .,]*$/,
+        len: [5,24]
+      }
+    },
+    code_post: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        is: /^[0-9]*$/,
+        len: [3,10]
+      }
+    },
+    detail_address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        is: /^[a-zA-Z .,]*$/,
+        len: [5,35]
+      }
+    },
+    nickname_address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        is: /^[a-zA-Z .,]*$/,
+        len: [5,10]
+      }
+    },
+    latitude: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        is: /^[0-9 .,]*$/,
+        len: [5,24]
+      }
+    },
+    longitude: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+        is: /^[0-9 .,]*$/,
+        len: [5,24]
+      }
+    }
   }, {
     sequelize,
     modelName: 'Address_Shipping',
